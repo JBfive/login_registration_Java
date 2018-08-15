7ea39d0bc0a0d5d893349fab505db3cb8055f58f
James Tuesday Paschall <jamestpaschall@Big-Brain.local>
2018-08-15 16:39:21 -0700

    first commit

---
+++ src/main/java/com/jym/authentication/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.jym.authentication.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jym.authentication.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email); 
+}
